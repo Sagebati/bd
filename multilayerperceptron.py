a4359d00b1cbd44a5cf581467ae7e303aa0073d9
TanguyHsrt <tanguyherserant@gmail.com>
2019-04-24 10:52:30 +0200

    multilayerperceptron work, need test on googlecloud

--- multilayerperceptron.py
+++ multilayerperceptron.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from pyspark.ml.classification import MultilayerPerceptronClassifier
+from pyspark.ml.classification import MultilayerPerceptronClassifier, LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-from pyspark.ml.feature import Word2Vec, Word2VecModel
-
+from pyspark.ml.feature import Word2Vec, Word2VecModel, StopWordsRemover, HashingTF, IDF, StringIndexer
+from pyspark.ml.pipeline import Pipeline
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 import numpy as np
@@ -18,42 +18,33 @@ spark = SparkSession(sc)
 
 df = spark.read.csv("Sentiment Analysis Dataset.csv", header=True)
 (train, test) = df.randomSplit([0.7, 0.3])
-#text_word = (i.split(" ") for i in df["SentimentText"].collect())
-#df.withColumn("SentimentText", df.select("SentimentText").split(" "))
-#text_word = df.rdd.map(lambda x: ("SentimentText").split(" ")).show()
-
-tokenizer = Tokenizer(inputCol="SentimentText", outputCol="words")
-tok = tokenizer.transform(df)
-df.show(20)
-tok.select("SentimentText", "words").show(20)
-train_texts = train.select("SentimentText").rdd.flatMap(lambda x: x).collect()
-train_labels = train.select("Sentiment").rdd.flatMap(lambda x: x).collect()
-
-test_texts = test.select("SentimentText").rdd.flatMap(lambda x: x).collect()
-test_labels = test.select("Sentiment").rdd.flatMap(lambda x: x).collect()
+train.show(20)
+tokenizer = Tokenizer(inputCol="SentimentText", outputCol="tokens")
+stop_remover = StopWordsRemover(inputCol="tokens", outputCol="words")
+label_stringIdx = StringIndexer(inputCol="Sentiment", outputCol="label")
 
 # Utilisation de Word2vec
 file = "tokenSave2"
 if not os.path.exists(file):
     word2Vec = Word2Vec(inputCol="words", outputCol="SentimentTextTransform")
-    word2Vec = word2Vec.fit(tok.select("words"))
+    word2Vec = word2Vec.fit(train)
     word2Vec.write().overwrite().save(file)
 else:
     word2Vec = Word2VecModel.load(file)
 
-result = word2Vec.transform(tok.select("words"))
-result.show(20)
-word2Vec.getVectors().show()
-split = result.select("words").randomSplit([0.6, 0.4])
-train = split[0]
-test = split[1]
-mlp = MultilayerPerceptronClassifier(maxIter=100, layers=[256, 128, 2], blockSize=1, seed=123)
-train = train.withColumnRenamed('words', 'features').collect()
-model = mlp.fit(train)
+#mlp = LogisticRegression(maxIter=10, regParam=0.01, featuresCol ="SentimentTextTransform")
+mlp = MultilayerPerceptronClassifier(maxIter=100, layers=[2, 3, 3, 2],
+                                     blockSize=1, seed=123, featuresCol="SentimentTextTransform",
+                                     predictionCol="prediction")
 
-test = test.withColumnRenamed('words', 'features').collect()
+pipeline = Pipeline(stages=[tokenizer, stop_remover, word2Vec, label_stringIdx, mlp])
+model = pipeline.fit(train)
 
 result = model.transform(test)
-predictionAndLabels = result.select("prediction", "Sentiment")
+result.show()
+model.write().overwrite().save("mpc")
+
+predictionAndLabels = result.select("prediction", "label")
+
 evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
 print("Test set accuracy = " + str(evaluator.evaluate(predictionAndLabels)))
